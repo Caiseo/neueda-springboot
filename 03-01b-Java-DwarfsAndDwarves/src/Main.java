8e942047b3df041e198d47071372220e4b9a68a5
MCC17149885 <cmccrory11@qub.ac.uk>
2017-11-28 13:28:51 +0000

    Another commit

---
+++ 03-01b-Java-DwarfsAndDwarves/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ArrayList<Dwarf> dwarfs = new ArrayList<Dwarf>();
+
+Dwarf sleepy = new Dwarf();
+sleepy.setName("Sleepy");
+sleepy.setAuthor("Disney");
+sleepy.setImage("Sleepy.png");
+
+Dwarf happy = new Dwarf();
+happy.setName("Happy");
+happy.setAuthor("Disney");
+happy.setImage("Happy.png");
+
+Dwarf dopey = new Dwarf();
+dopey.setName("Dopey");
+dopey.setAuthor("Disney");
+dopey.setImage("Dopey.png");
+	}
+
+}
