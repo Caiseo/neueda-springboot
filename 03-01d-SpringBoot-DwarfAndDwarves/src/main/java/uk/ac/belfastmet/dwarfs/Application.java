67d554835181986ebc5ec416cba31796208f548f
MCC17149885 <cmccrory11@qub.ac.uk>
2017-11-28 16:21:28 +0000

    Afternoon Commit on controllers

--- 03-01d-SpringBoot-DwarfAndDwarves/src/main/java/uk/ac/belfastmet/dwarfs/Application.java
+++ 03-01d-SpringBoot-DwarfAndDwarves/src/main/java/uk/ac/belfastmet/dwarfs/Application.java
@@ -1,12 +1,84 @@
 package uk.ac.belfastmet.dwarfs;
 
+import java.util.ArrayList;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import uk.ac.belfastmet.dwarfs.domain.Dwarf;
 
 @SpringBootApplication
 public class Application {
 
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+@PostConstruct
+public void createDwarfs() {
+				// Creating Dwarfs using a constructor
+			ArrayList<Dwarf> dwarfs = new ArrayList<Dwarf>();
+			ArrayList<Dwarf> dwarves = new ArrayList<Dwarf>();
+
+			Dwarf sleepy = new Dwarf("Sleepy", "Disney", "Sleepy.png");
+			dwarfs.add(sleepy);
+			Dwarf happy = new Dwarf("Happy", "Disney", "Happy.png");
+			dwarfs.add(happy);
+			Dwarf dopey = new Dwarf("Dopey", "Disney", "Dopey.png");
+			dwarfs.add(happy);
+			Dwarf bashful = new Dwarf("Bashful", "Disney", "Bashful.png");
+			dwarfs.add(bashful);
+			Dwarf grumpy = new Dwarf("Grumpy", "Disney", "Grumpy.png");
+			dwarfs.add(grumpy);
+			Dwarf doc = new Dwarf("Doc", "Disney", "Doc.png");
+			dwarfs.add(doc);
+			Dwarf sneezy = new Dwarf("Sneezy", "Disney", "Sneezy.png");
+			dwarfs.add(sneezy);
+
+			Dwarf thorin = new Dwarf("Thorin", "Tolkien", "Thorin.png");
+			dwarves.add(thorin);
+			Dwarf balin = new Dwarf("Balin", "Tolkien", "Balin.png");
+			dwarves.add(balin);
+			Dwarf dwalin = new Dwarf("Dwalin", "Tolkien", "Dwalin.png");
+			dwarves.add(dwalin);
+			Dwarf fili = new Dwarf("Fíli", "Tolkien", "Fíli.png");
+			dwarves.add(fili);
+			Dwarf kili = new Dwarf("Kíli", "Tolkien", "Kíli.png");
+			dwarves.add(kili);
+			Dwarf dori = new Dwarf("Dori", "Tolkien", "Dori.png");
+			dwarves.add(dori);
+			Dwarf nori = new Dwarf("Nori", "Tolkien", "Nori.png");
+			dwarves.add(nori);
+			Dwarf ori = new Dwarf("Ori", "Tolkien", "Ori.png");
+			dwarves.add(ori);
+			Dwarf oin = new Dwarf("Óin", "Tolkien", "Óin.png");
+			dwarves.add(oin);
+			Dwarf gloin = new Dwarf("Glóin", "Tolkien", "Glóin.png");
+			dwarves.add(gloin);
+			Dwarf bifur = new Dwarf("Bifur", "Tolkien", "Bifur.png");
+			dwarves.add(bifur);
+			Dwarf bofur = new Dwarf("Bofur", "Tolkien", "Bofur.png");
+			dwarves.add(bofur);
+			Dwarf bombur = new Dwarf("Bombur", "Tolkien", "Bombur.png");
+			dwarves.add(bombur);
+
+			System.out.println("Snow White's Dwarfs are:");
+			for (Dwarf dwarf : dwarfs) {
+				System.out.println(dwarf.getName() + " : " + dwarf.getAuthor() + " : " + dwarf.getImage());
+			}
+
+			System.out.println();
+			System.out.println("The Company of Tolkien Dwarves are:");
+			for (Dwarf dwarf : dwarves) {
+				System.out.println(dwarf.getName() + " : " + dwarf.getAuthor() + " : " + dwarf.getImage());
+			}
+		}
+
+
+	
 }
+
