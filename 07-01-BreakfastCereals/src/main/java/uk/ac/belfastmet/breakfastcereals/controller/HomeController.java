c9f440db70916e5b7793c7cb560c69a859c9d0f4
MCC17149885 <cmccrory11@qub.ac.uk>
2017-12-12 15:12:46 +0000

    Edit / Add button working.

--- 07-01-BreakfastCereals/src/main/java/uk/ac/belfastmet/breakfastcereals/controller/HomeController.java
+++ 07-01-BreakfastCereals/src/main/java/uk/ac/belfastmet/breakfastcereals/controller/HomeController.java
@@ -1,8 +1,11 @@
 package uk.ac.belfastmet.breakfastcereals.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,44 +18,47 @@ import uk.ac.belfastmet.breakfastcereals.repositories.CerealRepository;
 @RequestMapping("/")
 
 public class HomeController {
-	
+
 	@Autowired
 	CerealRepository cerealRepository;
-	
+
 	@GetMapping("/")
 	public String home(Model model) {
 		model.addAttribute("pageTitle", "Home");
 		model.addAttribute("cereals", cerealRepository.findAll());
 		return "homePage";
 
-}
+	}
 	@GetMapping("/cereal/view/{cerealId}")
 	public String viewCereal(@PathVariable("cerealId")Integer cerealId, Model model) {
 		model.addAttribute("pageTitle", "View Cereal");
 		model.addAttribute("cereal", cerealRepository.findOne(cerealId));
 		return "viewCereal";
-		
+
 	}
-	
+
 	@GetMapping("/cereal/add")
 	public String createCereal(Model model) {
 		model.addAttribute("pageTitle", "Add Cereal");
 		model.addAttribute("cereal", new Cereal());
 		return "editCereal";
-				
+
 	}
-	
+
 	@PostMapping("/cereal/save")
-	public String saveCereal(Cereal cereal) {
-		
-		Cereal savedCereal = cerealRepository.save(cereal);
-		return "redirect:/cereal/view/" + savedCereal.getCerealId();
-		
-	}
-	
-	
-	
-	
-	
-	
+	public String saveCereal(@Valid Cereal cereal, BindingResult bindingResult, Model model) {
+
+		if(bindingResult.hasErrors()) {
+			return "editCereal";
+
+		} else {
+
+			Cereal savedCereal = cerealRepository.save(cereal);
+			return "redirect:/cereal/view/" + savedCereal.getCerealId();
+
+		}
+
+	}	
+
+
 }
