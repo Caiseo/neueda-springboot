2f640f0c61e3ea3a189dab3fd2f98e29ddd6261a
MCC17149885 <cmccrory11@qub.ac.uk>
2017-12-12 09:46:00 +0000

    Start of Cereal project

---
+++ 07-01-BreakfastCereals/src/main/java/uk/ac/belfastmet/breakfastcereals/repositories/CerealRepository.java
@@ -0,0 +1,8 @@
+package uk.ac.belfastmet.breakfastcereals.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import uk.ac.belfastmet.breakfastcereals.domain.Cereal;
+
+public interface CerealRepository extends CrudRepository<Cereal, Integer> {
+}
